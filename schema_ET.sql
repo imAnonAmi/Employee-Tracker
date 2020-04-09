51384916600ee5519a462687e855380c06a7d69f
imAnonAmi <mcintireaw@gmail.com>
2020-04-09 09:52:54 -0700

    Basic database and tables written. Populate with seed data next.

---
+++ schema_ET.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS Employee_Tracker;
+
+CREATE DATABASE Employee_Tracker;
+
+USE Employee_Tracker
+
+CREATE TABLE department(
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY(id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
