62958e09482912c7d7851e0da13687fb1deba400
imAnonAmi <mcintireaw@gmail.com>
2020-04-09 13:26:03 -0700

    Wrote main menu function in entirety, and built out architecture for remaining functions as well as syntax examples.

--- app_ET.js
+++ app_ET.js
@@ -1,20 +1,85 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
 const connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+// Your port; if not 3306
     port: 3306,
 
-    // Your username
+// Username set to root
     user: "root",
 
-    // Your password
+// Your password. Make sure to ENTER YOUR PASSWORD between "" in order for the app to function properly.
     password: "",
     database: "employee_tracker"
     });
 
     connection.connect(function(err) {
     if (err) throw err;
+
 runSearch();
     });
+
+// Main menu functionality
+    function runSearch() {
+        console.log("Welcome to Employee Tracker 3000.");
+        inquirer
+          .prompt({
+            name: "action",
+            type: "list",
+            message: "Please select from the following options:",
+            choices: [
+                "Review Departments",
+                "Review Roles",
+                "Review Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update an Employee Role",
+                "Exit Application"
+            ]
+          })
+          .then(function(answer) {
+            switch (answer.action) {
+
+// Don't forget to use connection.query for the below, and to use "back ticks" if appropriate
+
+// Call reviewDepartment which will essentially be: SELECT * FROM department
+            case "Review Departments":
+              reviewDepartment();
+              break;
+// Call reviewRole which will essentially be: SELECT * FROM role     
+            case "Review Roles":
+              reviewRole();
+              break;
+// Call reviewEmployee which will essentially be: SELECT * FROM employee    
+            case "Review Employees":
+              reviewEmployee();
+              break;
+// Call addDepartment which will essentially be: INSERT INTO department (name) VALUES ("XXX"),     
+            case "Add Department":
+              addDepartment();
+              break;
+// Call addRole which will essentially be: INSERT INTO role (title, salary, department_id) VALUES ("Sales Team Leader", 45000, 1)     
+            case "Add Role":
+              addRole();
+              break;
+// Call addEmployee which will essentially be: INSERT INTO employee (first_name, last_name, role_id) VALUES ("Trevor", "Bruttenholm", 5)
+            case "Add Employee":
+              addEmployee();
+              break;
+// Call updateEmployeeRole which will essentially be: UPDATE employee SET role = ? WHERE id = ?
+// !!! BE SURE AND REVIEW ICE CREAM CRUD AND IN CLASS WORK ON THIS FOR CONTEXT. IT'S LIKE THE PROJECT WE WORKED ON EARLIER SYNTAX WISE.
+            case "Update an Employee Role":
+              updateEmployeeRole();
+              break;
+
+            case "Exit Application":
+              return;
+              
+            }
+          });
+      }
+// End of runSearch Function. Whew!
+// Write other functions below, have them close by calling runSearch to return to Main Menu for validation.
