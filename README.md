b401dbbcfe11983fdaa5e7e2a7e85e5d1182a420
imAnonAmi <mcintireaw@gmail.com>
2020-04-10 10:29:37 -0700

    Cleaned up commenting and formatting. Readme.md drafted

--- README.md
+++ README.md
@@ -13,27 +13,37 @@ Homework 12 assignment
 * Questions
 
 ## Description
+This a simple organizational CMS, that allows the user to manage a variety of staff administration functions utilizing a CLI. Within this application, the user navigates a menu list of options which enable them to perform the following functions:
 
+  * Add departments, roles, and employees
+
+  * Review departments, roles, and employees
+
+  * Update employee roles
 
 ## Installation
-Deployed at: 
+Repository at: https://github.com/imAnonAmi/Employee-Tracker
 
-If downloaded locally:
+Once downloaded locally:
 
 1. From your terminal, navigate to the proper folder:
 
-XXXXXX
+/employee-tracker/
 
 2. When you have navigated to this folder in your terminal, type the following command to load all dependencies:
 
 npm i
 
-3. When all npm has finished installing dependencies, type the following command (still in the same folder) to run app:
+3. Utilizing MySQL Workbench, create the required database and table using the schema_ET.sql file. You can then run the seed_ET.sql file in MySQL Workbench, which will auto-populate some starting data to work with.
 
-XXXXXX
+4. IMPORTANT! Within the app_ET.js file, you will need to enter your password where indicated and save the file in order for the app to work.
+
+5. When you have completed all of the above type the following command (still inside the /employee-tracker folder) to run app:
+
+node app_ET.js
 
 ## Usage
-
+Once the user has launched the app, they can make a variety of choices utilizing the main menu and subsequent prompts. Selections are all driven by IDs, which can be reviewed prior to making any changes using the appropriate main menu options (Review Department/Role/Employee).
 
 ## Known Issues
 
@@ -52,3 +62,4 @@ imAnonAmi
 N/A
 
 ## Questions and Further Thoughts
+
--- app_ET.js
+++ app_ET.js
@@ -2,239 +2,235 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-// Your port; if not 3306
-    port: 3306,
 
-// Username set to root
-    user: "root",
+  port: 3306,
+  user: "root",
 
-// Your password. Make sure to ENTER YOUR PASSWORD between "" in order for the app to function properly.
-    password: "",
-    database: "employee_tracker"
+  // Make sure to ENTER YOUR PASSWORD below in-between the "" in order for the app to function.
+  password: "",
+  database: "employee_tracker"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+
+  runSearch();
+});
+
+// Main menu functionality is contained in runSearch. All other functions end by bringing user back here to perform additional functions or exit application.
+function runSearch() {
+  console.log("Welcome to the Employee Tracker 3000.");
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "Please select from the following options:",
+      choices: [
+        "Review Departments",
+        "Review Roles",
+        "Review Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update an Employee Role",
+        "Exit Application"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+
+        // Call reviewDepartment which will essentially be: SELECT * FROM department
+        case "Review Departments":
+          reviewDepartment();
+          break;
+        // Call reviewRole which will essentially be: SELECT * FROM role     
+        case "Review Roles":
+          reviewRole();
+          break;
+        // Call reviewEmployee which will essentially be: SELECT * FROM employee    
+        case "Review Employees":
+          reviewEmployee();
+          break;
+        // Call addDepartment which will essentially be: INSERT INTO department (name) VALUES ("XXX")     
+        case "Add Department":
+          addDepartment();
+          break;
+        // Call addRole which will essentially be: INSERT INTO role (title, salary, department_id) VALUES ("Sales Team Leader", 45000, 1)     
+        case "Add Role":
+          addRole();
+          break;
+        // Call addEmployee which will essentially be: INSERT INTO employee (first_name, last_name, role_id) VALUES ("Trevor", "Bruttenholm", 5)
+        case "Add Employee":
+          addEmployee();
+          break;
+        // Call updateEmployeeRole which will essentially be: UPDATE employee SET role = ? WHERE id = ?
+        // !!! BE SURE AND REVIEW ICE CREAM CRUD AND IN CLASS WORK ON THIS FOR CONTEXT. IT'S LIKE THE PROJECT WE WORKED ON EARLIER SYNTAX WISE.
+        case "Update an Employee Role":
+          updateEmployeeRole();
+          break;
+        // Terminates SQL connection and returns out of JS, leaving user at command prompt.
+        case "Exit Application":
+          console.log("Goodbye diligent employee stalker!");
+          connection.end();
+          return;
+
+      }
     });
-
-    connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-
-runSearch();
-    });
-
-// Main menu functionality
-    function runSearch() {
-        console.log("Welcome to Employee Tracker 3000.");
-        inquirer
-          .prompt({
-            name: "action",
-            type: "list",
-            message: "Please select from the following options:",
-            choices: [
-                "Review Departments",
-                "Review Roles",
-                "Review Employees",
-                "Add Department",
-                "Add Role",
-                "Add Employee",
-                "Update an Employee Role",
-                "Exit Application"
-            ]
-          })
-          .then(function(answer) {
-            switch (answer.action) {
-
-// Don't forget to use connection.query for the below, and to use "back ticks" if appropriate
-
-// Call reviewDepartment which will essentially be: SELECT * FROM department
-            case "Review Departments":
-              reviewDepartment();
-              break;
-// Call reviewRole which will essentially be: SELECT * FROM role     
-            case "Review Roles":
-              reviewRole();
-              break;
-// Call reviewEmployee which will essentially be: SELECT * FROM employee    
-            case "Review Employees":
-              reviewEmployee();
-              break;
-// Call addDepartment which will essentially be: INSERT INTO department (name) VALUES ("XXX")     
-            case "Add Department":
-              addDepartment();
-              break;
-// Call addRole which will essentially be: INSERT INTO role (title, salary, department_id) VALUES ("Sales Team Leader", 45000, 1)     
-            case "Add Role":
-              addRole();
-              break;
-// Call addEmployee which will essentially be: INSERT INTO employee (first_name, last_name, role_id) VALUES ("Trevor", "Bruttenholm", 5)
-            case "Add Employee":
-              addEmployee();
-              break;
-// Call updateEmployeeRole which will essentially be: UPDATE employee SET role = ? WHERE id = ?
-// !!! BE SURE AND REVIEW ICE CREAM CRUD AND IN CLASS WORK ON THIS FOR CONTEXT. IT'S LIKE THE PROJECT WE WORKED ON EARLIER SYNTAX WISE.
-            case "Update an Employee Role":
-              updateEmployeeRole();
-              break;
-
-            case "Exit Application":
-              console.log("Goodbye diligent employee stalker!");
-              connection.end();
-              return;
-              
-            }
-          });
-      };
-// End of runSearch Function. Whew!
-
+};
 // --------------------------------------------------
-// Write other functions below, have them close by calling runSearch to return to Main Menu for validation.
+
+
+// Individual functions (main menu options)
 
 // reviewDepartment which will essentially be: SELECT * FROM department
 function reviewDepartment() {
-    console.log("Showing all departments:\n");
-    connection.query("SELECT * FROM department", function(err, res) {
-      if (err) throw err;
-      
-      console.table(res);
-      runSearch();
-    });
-  };
+  console.log("Showing all departments:\n");
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    runSearch();
+  });
+};
 // reviewRole which will essentially be: SELECT * FROM role
 function reviewRole() {
-    console.log("Showing all roles:\n");
-    connection.query("SELECT * FROM role", function(err, res) {
-      if (err) throw err;
-      
-      console.table(res);
-      runSearch();
-    });
-  };
+  console.log("Showing all roles:\n");
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    runSearch();
+  });
+};
 // reviewEmployee which will essentially be: SELECT * FROM employee
 function reviewEmployee() {
-    console.log("Showing all employees:\n");
-    connection.query("SELECT * FROM employee", function(err, res) {
-      if (err) throw err;
-      
-      console.table(res);
-      runSearch();
-    });
-  };
+  console.log("Showing all employees:\n");
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    runSearch();
+  });
+};
 // addDepartment which will essentially be: INSERT INTO department (name) VALUES ("Acquisitions")
 function addDepartment() {
 
-    inquirer
-          .prompt({
-            name: "newDepartment",
-            type: "input",
-            message: "Please enter the name of the department you want to add:",
-            
-          })
-          .then(function(answer) {
-          let newDpt = answer.newDepartment;
-          connection.query(`INSERT INTO department (name) VALUES ("${newDpt}")`, function (err, res) { 
+  inquirer
+    .prompt({
+      name: "newDepartment",
+      type: "input",
+      message: "Please enter the name of the department you want to add:",
 
-      if (err) throw err;
-      
-      console.log("A new department has been created.");
-      runSearch();
+    })
+    .then(function (answer) {
+      let newDpt = answer.newDepartment;
+      connection.query(`INSERT INTO department (name) VALUES ("${newDpt}")`, function (err, res) {
+
+        if (err) throw err;
+
+        console.log("A new department has been created.");
+        runSearch();
+      });
     });
-  });
 };
 // addRole which will essentially be: INSERT INTO role (title, salary, department_id) VALUES ("Sales Team Leader", 45000, 1)
 async function addRole() {
 
-    inquirer
-          .prompt([{
-            name: "newTitle",
-            type: "input",
-            message: "Please enter the TITLE of the new role you want to create:"
-            
-          },
-          {
-            name: "newSalary",
-            type: "number",
-            message: "Please enter the SALARY for this new role (Input must be a number with NO punctuation):"
-            
-          },
-          {
-            name: "newDeptID",
-            type: "number",
-            message: "Please enter the DEPARTMENT ID for this role (If you do not know your Department ID, you can look it up using Review Departments on the Main Menu.):"
-            
-          }])
-          .then(function(answer) {
-          let newTitle = answer.newTitle;
-          let newSalary = answer.newSalary;
-          let newDeptID = answer.newDeptID;
-          connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${newTitle}", ${newSalary}, ${newDeptID})`, function (err, res) { 
+  inquirer
+    .prompt([{
+      name: "newTitle",
+      type: "input",
+      message: "Please enter the TITLE of the new role you want to create:"
 
-      if (err) throw err;
-      
-      console.log("A new role has been created.");
-      runSearch();
+    },
+    {
+      name: "newSalary",
+      type: "number",
+      message: "Please enter the SALARY for this new role (Input must be a number with NO punctuation):"
+
+    },
+    {
+      name: "newDeptID",
+      type: "number",
+      message: "Please enter the DEPARTMENT ID for this role (If you do not know your Department ID, you can look it up using Review Departments on the Main Menu.):"
+
+    }])
+    .then(function (answer) {
+      let newTitle = answer.newTitle;
+      let newSalary = answer.newSalary;
+      let newDeptID = answer.newDeptID;
+      connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${newTitle}", ${newSalary}, ${newDeptID})`, function (err, res) {
+
+        if (err) throw err;
+
+        console.log("A new role has been created.");
+        runSearch();
+      });
     });
-  });
 };
 // addEmployee which will essentially be: INSERT INTO employee (first_name, last_name, role_id) VALUES ("Trevor", "Bruttenholm", 6)
 async function addEmployee() {
 
-    inquirer
-          .prompt([{
-            name: "firstName",
-            type: "input",
-            message: "Please enter the new employee's FIRST name:"
-            
-          },
-          {
-            name: "lastName",
-            type: "input",
-            message: "Please enter the new employee's LAST name:"
-            
-          },
-          {
-            name: "roleID",
-            type: "number",
-            message: "Please enter the ROLE ID for this new employee(If you do not know the Role ID, you can look it up using Review Roles on the Main Menu.):"
-            
-          }])
-          .then(function(answer) {
-          let firstName = answer.firstName;
-          let lastName = answer.lastName;
-          let roleID = answer.roleID;
-          connection.query(`INSERT INTO employee (first_name, last_name, role_id) VALUES ("${firstName}", "${lastName}", ${roleID})`, function (err, res) { 
+  inquirer
+    .prompt([{
+      name: "firstName",
+      type: "input",
+      message: "Please enter the new employee's FIRST name:"
 
-      if (err) throw err;
-      
-      console.log("A new role has been created.");
-      runSearch();
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "Please enter the new employee's LAST name:"
+
+    },
+    {
+      name: "roleID",
+      type: "number",
+      message: "Please enter the ROLE ID for this new employee(If you do not know the Role ID, you can look it up using Review Roles on the Main Menu.):"
+
+    }])
+    .then(function (answer) {
+      let firstName = answer.firstName;
+      let lastName = answer.lastName;
+      let roleID = answer.roleID;
+      connection.query(`INSERT INTO employee (first_name, last_name, role_id) VALUES ("${firstName}", "${lastName}", ${roleID})`, function (err, res) {
+
+        if (err) throw err;
+
+        console.log("A new role has been created.");
+        runSearch();
+      });
     });
-  });
 };
 // updateEmployeeRole which will essentially be: UPDATE employee SET role = ? WHERE id = ?
 async function updateEmployeeRole() {
 
   inquirer
-        .prompt([{
-          name: "empID",
-          type: "number",
-          message: "Please enter the EMPLOYEE ID for the employee whose role you would like to modify (If you do not know the Employee ID, you can look it up using Review Employees on the Main Menu.):"
-          
-        },
-        {
-          name: "roleID",
-          type: "number",
-          message: "Please enter the ROLE ID for this new employee (If you do not know the Role ID, you can look it up using Review Roles on the Main Menu.):"
-          
-        }])
-        .then(function(answer) {
-        let empID = answer.empID;
-        let roleID = answer.roleID;
-        connection.query(`UPDATE employee SET role_id = ${roleID} WHERE id = ${empID}`, function (err, res) { 
+    .prompt([{
+      name: "empID",
+      type: "number",
+      message: "Please enter the EMPLOYEE ID for the employee whose role you would like to modify (If you do not know the Employee ID, you can look it up using Review Employees on the Main Menu.):"
 
-    if (err) throw err;
-    
-    console.log("Employee role has been modified.");
-    runSearch();
-  });
-});
+    },
+    {
+      name: "roleID",
+      type: "number",
+      message: "Please enter the ROLE ID for this new employee (If you do not know the Role ID, you can look it up using Review Roles on the Main Menu.):"
+
+    }])
+    .then(function (answer) {
+      let empID = answer.empID;
+      let roleID = answer.roleID;
+      connection.query(`UPDATE employee SET role_id = ${roleID} WHERE id = ${empID}`, function (err, res) {
+
+        if (err) throw err;
+
+        console.log("Employee role has been modified.");
+        runSearch();
+      });
+    });
 };
